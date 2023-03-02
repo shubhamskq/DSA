f05b35a3c3f228078e8e780ad9f59dc41c46e086
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-03-02 14:03:58 +0530

    Create Print name 5 times using recursion.cpp

---
+++ Recursion/Print name 5 times using recursion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+        void res(int n){
+          if(n == 5)
+              return;
+              cout << "Shubham" << endl;
+              res(n+1);
+        }
+int main() {
+       int n = 0;
+       res(n);
+    return 0;
+}
